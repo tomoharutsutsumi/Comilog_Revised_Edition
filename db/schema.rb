3d433bcce7eea4b7a388f72e234bdd11d48ad92b
tomoharu tsutsumi <>
2018-05-04 15:17:14 +0900

    add likeテーブル作成
    likeテーブル作成　カラムはcomic_id user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503054524) do
+ActiveRecord::Schema.define(version: 20180504061012) do
 
   create_table "comics", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20180503054524) do
     t.integer "day"
     t.string "origin_title"
     t.integer "agetarget"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "comic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
