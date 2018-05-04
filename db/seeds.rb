d8ac1063a3f03363844562795d7ff8f4836decbe
tomoharu tsutsumi <>
2018-05-04 15:43:26 +0900

    add seeds.rbデータ追加
    テスト用データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+13.times do |i|
+  Comic.create(title: "テスト#{i}")
+end
