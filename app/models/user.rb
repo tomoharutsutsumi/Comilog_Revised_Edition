1c2492fffcb866d29228cb802de1e21add9d3774
tomoharu tsutsumi <>
2018-05-04 16:25:41 +0900

    update　devise変更
    ログイン時にメール確認が不必要なため、confirmableを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_many :likes
+  has_many :comics
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
+         :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
 end
