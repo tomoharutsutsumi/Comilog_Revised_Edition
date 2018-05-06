d9ec2d62003def291336024bf030564f5cd61d1c
tomoharu tsutsumi <>
2018-05-06 19:01:27 +0900

    update comicモデル内にliked_byメソッドを作成
    userにcurrent_user.idを入れれば動くと思います。とりあえずrails cでは動きました。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'comics#index'
   resources :users
   resources :comics do
+    collection do
+        get 'rank'
+    end
     member do
         post 'like'
         post 'unlike'
