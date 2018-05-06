28b0f0e8a0a713fcb37fa730a91498e776eb0cb0
tomoharu tsutsumi <>
2018-05-06 17:12:08 +0900

    add 自分がいいねしたcomic一覧ページを作る
    2018/05/06時点でunable autoload　エラーが発生中。

---
+++ app/controllers/my/likes_controller.rb
@@ -0,0 +1,10 @@
+class LikesController < ApplicationController
+
+  def liked
+    like_ids = Like.where(user_id: current_user.id).pluck(:comic_id)
+    @my_liked_comics = Comic.where(id: like_ids)
+  end
+
+
+end
+
