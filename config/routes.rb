6688f44465698fea03dc9d2c7ce6362f0692fff1
tomoharu tsutsumi <>
2018-05-12 16:06:09 +0900

    Merge branch '自分がいいねしたcomic一覧画面を作る'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   devise_for :users
   root 'comics#index'
@@ -10,7 +9,7 @@ Rails.application.routes.draw do
     end
   end
   namespace :my do
-    get '/likes',:to => "likes#liked"
+    get '/likes',:to => 'likes#liked'
   end
 
 
