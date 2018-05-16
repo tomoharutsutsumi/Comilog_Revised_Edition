1b0ac00dbc6aa2880005d78e593ef0cd2ca5d09f
tomoharu tsutsumi <>
2018-05-16 21:16:31 +0900

    add comicモデルにstatusのenum指定
    releaseは0 nonreleaseは１で表す

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,7 +2,7 @@ class Comic < ApplicationRecord
   has_many :likes
   belongs_to :user
 
-
+  enum status:{release: 0, nonrelease: 1}
 
   def liked?(user)
     Like.exists?(user_id: user, comic_id: self.id)
