d9683046fa385312dd7edf4fab43ac8f8349d2c7
tomoharu tsutsumi <>
2018-05-17 21:43:28 +0900

    add いいねしたコミックの合計金額表示
    seedで一度データ入れ直しました。

--- app/controllers/my/likes_controller.rb
+++ app/controllers/my/likes_controller.rb
@@ -3,7 +3,12 @@ class My::LikesController < ApplicationController
 
   def liked
     @my_liked_comics =  Comic.joins({:likes => :user}).where("likes.user_id" => current_user.id)
+    @my_liked_comics.each do |comic|
+      @sum_of_my_liked_comics_price = 0
+      @sum_of_my_liked_comics_price += comic.price
+    end
   end
+
 
 
 end
