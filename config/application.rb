98fd3b3831752e8ed33efb581d9b1de3fda621c0
tomoharu tsutsumi <>
2018-06-20 22:56:41 +0900

    remove initializerにi18nを統一するため、削除。

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module ComilogRevisedEdition
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     #config.logger = Logger.new(STDOUT)
-    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
