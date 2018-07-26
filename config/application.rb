7c94a77e9b0ab1e0ea14e14fd4e2088d9c10fb2a
tomoharu tsutsumi <>
2018-07-26 23:42:30 +0900

    update ログイン画面にフッターを出さないようにする　

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,12 @@ module ComilogRevisedEdition
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+      Devise::PasswordsController.layout "devise" 
+    end
   end
 end
