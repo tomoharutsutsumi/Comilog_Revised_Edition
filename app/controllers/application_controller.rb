ca126fd8d88ca42e63d3f3f962e03bd1ab6a18a9
tomoharu tsutsumi <>
2018-07-29 18:36:50 +0900

    fix ログイン時のユーザー不正エラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     #strong parametersを設定し、usernameを許可
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
+         :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter], authentication_keys: [:username]
 
 
 
